358062da5bfaa1055d02c5677acc177541f3d9ce
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-09-29 11:26:59 +0100

    renamed functions and property

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,20 @@
-const message = document.querySelector('#message p');
-const messages = [
-  'The journey of a thousand miles begins with one step. - Lao Tzu',
-  'That which does not kill us makes us stronger. - Friedrich Nietzsche',
-  'Life is what happens when you’re busy making other plans. - John Lennon',
-  'When the going gets tough, the tough get going. - Joe Kennedy',
-  'You must be the change you wish to see in the world. - Mahatma Gandhi',
-  'You only live once, but if you do it right, once is enough. - Mae West',
-  'Tough times never last but tough people do. - Robert H. Schuller',
-  'Whether you think you can or you think you can’t, you’re right. - Henry Ford'
-]
+const quote = document.querySelector('#quote p');
+const author = document.querySelector('#author p');
 
-const randomMessage = ( arr ) => {
-  let randomNumber = Math.floor( Math.random() * arr.length);
-
-  return arr[randomNumber];
+const randomQuote = ( arr ) => {
+  let index = Math.floor( Math.random() * arr.length);
+  return {
+    quote: arr[index].quote,
+    author: arr[index].author
+  }
 }
 
-const produceMessage = ( data ) => {
-  /** 
-   * Test to see if the button is selected
-   * console.log( event.target );
-  */
-  message.textContent = randomMessage( data );
+const produceMessage = ( data, output) => {
+  let theQuote = output(quotes);
+  quote.textContent = `${theQuote.quote}`;
+  author.textContent = `${theQuote.author}`;
 }
 
-produceMessage(messages);
+produceMessage(quotes, randomQuote);
+
+
