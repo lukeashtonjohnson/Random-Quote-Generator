60db12db4e61af606975f08999344b78f99fefb2
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-09-29 11:27:38 +0100

    add quotes.js to house the array of quotes

---
+++ scripts/quotes.js
@@ -0,0 +1,30 @@
+const quotes = [
+  {
+    quote: 'The journey of a thousand miles begins with one step.',
+    author: 'Lao Tzu'
+  },
+  {
+    quote: 'That which does not kill us makes us stronger.',
+    author: 'Friedrich Nietzsche'
+  },
+  {
+    quote: 'Life is what happens when you’re busy making other plans.',
+    author: 'John Lennon'
+  },
+  {
+    quote: 'When the going gets tough, the tough get going.',
+    author: 'Joe Kennedy'
+  },
+  {
+    quote: 'You must be the change you wish to see in the world.',
+    author: 'Mahatma Gandhi'
+  },
+  {
+    quote: 'You only live once, but if you do it right, once is enough.',
+    author: 'Mae West'
+  },
+  {
+    quote: 'Whether you think you can or you think you can’t, you’re right.',
+    author: 'Henry Ford'
+  },
+]
