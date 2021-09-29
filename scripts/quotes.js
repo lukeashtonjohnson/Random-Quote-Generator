204b444244d8ce15ec06c1f63f567f2a20361a5f
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-09-29 11:57:22 +0100

    add image key to all objects in the quotes array
    key values are the path to an image of the author

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,30 +1,37 @@
 const quotes = [
   {
     quote: 'The journey of a thousand miles begins with one step.',
-    author: 'Lao Tzu'
+    author: 'Lao Tzu',
+    image: 'https://images.gr-assets.com/authors/1435903703p8/2622245.jpg'
   },
   {
     quote: 'That which does not kill us makes us stronger.',
-    author: 'Friedrich Nietzsche'
+    author: 'Friedrich Nietzsche',
+    image: 'https://www.thefamouspeople.com/profiles/images/og-friedrich-nietzsche-128.jpg'
   },
   {
     quote: 'Life is what happens when you’re busy making other plans.',
-    author: 'John Lennon'
+    author: 'John Lennon',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/8/85/John_Lennon_1969_%28cropped%29.jpg'
   },
   {
     quote: 'When the going gets tough, the tough get going.',
-    author: 'Joe Kennedy'
+    author: 'Joe Kennedy',
+    image: 'https://images.quote.org/36675_Joseph%20P.%20Kennedy_0.jpg'
   },
   {
     quote: 'You must be the change you wish to see in the world.',
-    author: 'Mahatma Gandhi'
+    author: 'Mahatma Gandhi',
+    image: 'https://www.biography.com/.image/t_share/MTQ1MjMwNzY0NDkxOTQxMjcy/mahatma-gandhi---a-legacy-of-peace.jpg'
   },
   {
     quote: 'You only live once, but if you do it right, once is enough.',
-    author: 'Mae West'
+    author: 'Mae West',
+    image: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTE5NTU2MzE2NDAzODI4MjM1/mae-west-9528264-1-402.jpg'
   },
   {
     quote: 'Whether you think you can or you think you can’t, you’re right.',
-    author: 'Henry Ford'
+    author: 'Henry Ford',
+    image: 'https://www.thoughtco.com/thmb/5YdpABzersSFFWvGGZAMHCx67w4=/2641x2008/filters:fill(auto,1)/GettyImages-515461052-dd32da349fa5499fb777ce0214a956e8.jpg'
   },
 ]
