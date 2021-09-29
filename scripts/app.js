8f8550978da7c28634dc3d4ef8e91a9565006644
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-09-29 09:35:04 +0100

    select p inside div with #message using the DOM
    create array of quotes
    
    create randomMessage function to generate a random message from the arrat messages
    
    create function to change the text content of the p with #message to random message

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+const message = document.querySelector('#message p');
+const messages = [
+  'The journey of a thousand miles begins with one step. - Lao Tzu',
+  'That which does not kill us makes us stronger. - Friedrich Nietzsche',
+  'Life is what happens when you’re busy making other plans. - John Lennon',
+  'When the going gets tough, the tough get going. - Joe Kennedy',
+  'You must be the change you wish to see in the world. - Mahatma Gandhi',
+  'You only live once, but if you do it right, once is enough. - Mae West',
+  'Tough times never last but tough people do. - Robert H. Schuller',
+  'Whether you think you can or you think you can’t, you’re right. - Henry Ford'
+
+]
+
+const randomMessage = ( arr ) => {
+  let randomNumber = Math.floor( Math.random() * arr.length);
+
+  return arr[randomNumber];
+}
+
+
+const handleButtonClick = ( data ) => {
+  /*
+    Test to see if the button is selected
+    console.log( event.target );
+  */
+  // message.textContent = `hello world`;
+  message.textContent = randomMessage( data );
+}
+
+handleButtonClick(messages);
