0cf01d8e7b2679e50c83c23967252f890d2d21b1
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-09-29 11:59:42 +0100

    add DOM selector for #photo img
    add image key to the object returned by randomQuote function
    
    add output for the image of the author in produceMessage function

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,13 @@
 const quote = document.querySelector('#quote p');
 const author = document.querySelector('#author p');
+const photo = document.querySelector('#photo img');
 
 const randomQuote = ( arr ) => {
   let index = Math.floor( Math.random() * arr.length);
   return {
     quote: arr[index].quote,
-    author: arr[index].author
+    author: arr[index].author,
+    image: arr[index].image
   }
 }
 
@@ -13,6 +15,8 @@ const produceMessage = ( data, output) => {
   let theQuote = output(quotes);
   quote.textContent = `${theQuote.quote}`;
   author.textContent = `${theQuote.author}`;
+  theQuote.image ? photo.src =`${theQuote.image}` : photo.src = `https://via.placeholder.com/150`;
+  
 }
 
 produceMessage(quotes, randomQuote);
